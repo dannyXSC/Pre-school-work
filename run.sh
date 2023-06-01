69da55304e51e45a2ac990047b7a8b77f75fce2d
dannyXSC <874794016@qq.com>
2023-06-01 15:28:45 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-python main.py --data-path "/remote-home/share/course23/aicourse_dataset_final/" 
+python main.py \
+    --data-path "/remote-home/share/course23/aicourse_dataset_final/" \
+    --model "dino-vitb16" \
+    --batch-size 64 \
+    --epochs 50 \
+    --lr 1e-4 \
+    --weight-decay 0.01 \
+     --output_dir output/dino-vitb16
