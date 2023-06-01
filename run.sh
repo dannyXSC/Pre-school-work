56cd19997d570c03aaeb09fc98c21a3cbf7501b0
dannyXSC <874794016@qq.com>
2023-06-01 15:37:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
+model_name="resnet-50"
 python main.py \
     --data-path "/remote-home/share/course23/aicourse_dataset_final/" \
-    --model "dino-vitb16" \
+    --model ${model_name} \
     --batch-size 64 \
     --epochs 50 \
     --lr 1e-4 \
     --weight-decay 0.01 \
-     --output_dir output/dino-vitb16
+     --output_dir output/${model_name}
