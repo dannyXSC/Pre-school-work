ba0ab3fc85fbe664f3ec70d37e4c3d5fc8a213a8
dannyXSC <874794016@qq.com>
2023-06-09 22:47:31 +0800

    batch size 32

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 model_name="cait_xxs36_224"
-# train
 python main.py \
     --data-path "/remote-home/share/course23/aicourse_dataset_final/" \
     --model ${model_name} \
-    --batch-size 128 \
+    --batch-size 32 \
     --epochs 50 \
     --lr 1e-4 \
     --weight-decay 0.01 \
-    --output_dir output/${model_name}
+    --output_dir output/${model_name} \
