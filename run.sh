95237e962e89721e4dce644d3bc319f67db397d2
dannyXSC <874794016@qq.com>
2023-06-09 13:56:45 +0800

    修改main
    改回初始状态

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-model_name="regnetx_002"
+model_name="deit_tiny_patch16_224"
 python main.py \
     --data-path "/remote-home/share/course23/aicourse_dataset_final/" \
     --model ${model_name} \
@@ -6,4 +6,4 @@ python main.py \
     --epochs 50 \
     --lr 1e-4 \
     --weight-decay 0.01 \
-     --output_dir output/${model_name}
+    --output_dir output/${model_name}
--- run_test.sh
+++ run_test.sh
@@ -6,5 +6,5 @@ python main.py \
     --epochs 50 \
     --lr 1e-4 \
     --weight-decay 0.01 \
-     --output_dir output/${model_name} \
-     --test_only
+    --output_dir output/${model_name} \
+    --test_only
