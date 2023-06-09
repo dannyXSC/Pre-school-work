03f0e26c6e84915a516e6ac70cc8c08cdb520dcc
dannyXSC <874794016@qq.com>
2023-06-09 20:09:51 +0800

    update run.sh
    调整batch size

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,7 @@ model_name="cait_xxs36_224"
 python main.py \
     --data-path "/remote-home/share/course23/aicourse_dataset_final/" \
     --model ${model_name} \
-    --batch-size 64 \
+    --batch-size 128 \
     --epochs 50 \
     --lr 1e-4 \
     --weight-decay 0.01 \
