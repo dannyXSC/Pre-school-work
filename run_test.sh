9f56488b0d8f728a4414188d4c6ba7877481d4ec
dannyXSC <874794016@qq.com>
2023-07-11 14:58:07 +0800

    更改测试

--- run_test.sh
+++ run_test.sh
@@ -1,11 +1,12 @@
-model_name="ssl_resnet18"
+model_name="deit_tiny_patch16_224"
 python main.py \
     --data-path "/remote-home/share/course23/aicourse_dataset_final/" \
     --model ${model_name} \
     --batch-size 32 \
     --epochs 50 \
-    --lr 1e-4 \
     --weight-decay 0.01 \
     --output_dir output/${model_name} \
     --test_only \
     --resume "output/${model_name}/best_checkpoint.pth"
+
+#     --lr 1e-4 \
