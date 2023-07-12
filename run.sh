a01ea463a4208f3a687b67cd5366bc8af1c116df
dannyXSC <874794016@qq.com>
2023-07-12 10:49:23 +0800

    使用swin_tiny_patch4_window7_224

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-model_name="tnt_s_patch16_224"
+model_name="swin_tiny_patch4_window7_224"
 python main.py \
     --data-path "/remote-home/share/course23/aicourse_dataset_final/" \
     --model ${model_name} \
