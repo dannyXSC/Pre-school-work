dbadc76f9d09323f0e12e38ee57343c0dfd30c88
dannyXSC <874794016@qq.com>
2023-07-12 15:56:50 +0800

    为pit_b_224添加数据增强

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-model_name="gmixer_24_224"
+model_name="pit_b_224"
 python main.py \
     --data-path "/remote-home/share/course23/aicourse_dataset_final/" \
     --model ${model_name} \
@@ -6,10 +6,6 @@ python main.py \
     --epochs 50 \
     --weight-decay 0.01 \
     --output_dir output/${model_name} \
-
-
-
-
-#    --flip \
-#    --rotation
+    --flip \
+    --rotation
 
