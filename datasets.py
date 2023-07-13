e758b43625f4226c981a441e7824606fbb505c14
dannyXSC <874794016@qq.com>
2023-07-13 15:05:52 +0800

    1

--- datasets.py
+++ datasets.py
@@ -28,7 +28,8 @@ class MultiImageFolder(data.Dataset):
         self.transform = transform
 
         samples_list = [x.samples for x in dataset_list]
-        print("samples_list:".format(samples_list))
+        for item in samples_list:
+            print("samples_list:".format(item))
         classes_list = [x.classes for x in dataset_list]
         self.classes_list = classes_list
         self.dataset_list = dataset_list
