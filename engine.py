b5fa0f434fbc916167c0d292db9d7372b22b80fd
dannyXSC <874794016@qq.com>
2023-07-19 13:17:19 +0800

    增加

--- engine.py
+++ engine.py
@@ -35,7 +35,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
         if args.bce_loss:
             targets = targets.gt(0.0).type(targets.dtype)
         with torch.cuda.amp.autocast():
-            # samples = samples.reshape(samples.shape[0], 3, -1)
+            samples = samples.reshape(samples.shape[0], 3, -1)
             outputs = model(samples, dataset_ids)
             if class_indicator is not None:
                 mask = class_indicator[targets]
