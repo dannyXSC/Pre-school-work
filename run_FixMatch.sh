ac403b8a27c630cf5b6345e89ac2f54265f7fb63
dannyXSC <874794016@qq.com>
2023-07-21 15:17:31 +0800

    1

--- run_FixMatch.sh
+++ run_FixMatch.sh
@@ -2,7 +2,7 @@ model_name="FixMatch"
 python FixMatch_main.py \
     --data-path "/remote-home/share/course23/aicourse_dataset_final/" \
     --model ${model_name} \
-    --batch-size 32 \
+    --batch-size 16 \
     --epochs 50 \
     --weight-decay 0.01 \
     --output_dir output/${model_name} \
