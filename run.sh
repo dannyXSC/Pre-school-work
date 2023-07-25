fe30f4051d37c36aa3af967cff33f24678230962
dannyXSC <874794016@qq.com>
2023-07-25 23:16:05 +0800

    训练30轮

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ python main.py \
     --data-path "/remote-home/share/course23/aicourse_dataset_final/" \
     --model ${model_name} \
     --batch-size 32 \
-    --epochs 50 \
+    --epochs 30 \
     --weight-decay 0.01 \
     --output_dir output/${model_name} \
     --rotation 45 \
