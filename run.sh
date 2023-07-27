2c732f68c14c08341ab488e2ed615b6c4ee8e4c6
dannyXSC <874794016@qq.com>
2023-07-27 17:02:50 +0800

    run 20 epoch 并且采用最后一个时间点作为模型

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ python main.py \
     --data-path "/home/aicourse_dataset/" \
     --model ${model_name} \
     --batch-size 32 \
-    --epochs 50 \
+    --epochs 20 \
     --weight-decay 0.01 \
     --output_dir output/${model_name} \
     --rotation 45 \
     --flip 0.5 \
     --add_origin_image \
-    --split_dataset \
+#    --split_dataset \
 # pit_b_224
--- run_test.sh
+++ run_test.sh
@@ -3,9 +3,9 @@ python main.py \
     --data-path "/home/aicourse_dataset/" \
     --model ${model_name} \
     --batch-size 32 \
-    --epochs 50 \
+    --epochs 20 \
     --weight-decay 0.01 \
     --output_dir output/${model_name} \
     --test_only \
-    --resume "output/${model_name}/best_checkpoint.pth" \
+    --resume "output/${model_name}/checkpoint.pth" \
 #     --lr 1e-4 \
