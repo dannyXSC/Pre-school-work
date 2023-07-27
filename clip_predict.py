d82204c1378374b124b4e2e799d5071a050931a4
dannyXSC <874794016@qq.com>
2023-07-27 13:34:35 +0800

    添加template

--- clip_predict.py
+++ clip_predict.py
@@ -265,6 +265,30 @@ UN_LOCODE_Code_list = {
     "XK": "Kosovo"
 }
 
+templates = {
+    "Food101": ['a photo of {}, a type of food.'],
+    "CIFAR100": [
+        'a photo of a {}.',
+        'a blurry photo of a {}.',
+        'a black and white photo of a {}.',
+        'a low contrast photo of a {}.',
+        'a high contrast photo of a {}.',
+        'a bad photo of a {}.',
+        'a good photo of a {}.',
+        'a photo of a small {}.',
+        'a photo of a big {}.',
+        'a photo of the {}.',
+        'a blurry photo of the {}.',
+        'a black and white photo of the {}.',
+        'a low contrast photo of the {}.',
+        'a high contrast photo of the {}.',
+        'a bad photo of the {}.',
+        'a good photo of the {}.',
+        'a photo of the small {}.',
+        'a photo of the big {}.',
+    ],
+}
+
 
 def get_dirs(path):
     return [dir_name for dir_name in os.listdir(path) if
